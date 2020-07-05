ac8e3c9a9889ae4be00bc91bc818a08c92b8bdfc
dejavobobo <57739035+dejavobobo@users.noreply.github.com>
2020-07-05 13:09:46 -0500

    first commit

---
+++ src/main/java/seleniumConcept/WindowHandler.java
@@ -0,0 +1,56 @@
+package seleniumConcept;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+
+public class WindowHandler {
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	public void launchbrowser() {
+		
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.manage().window().maximize();
+		
+		driver.get("https://www.yahoo.com/");
+		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+	}
+	
+	@Test
+	public void loginTest1() throws InterruptedException {
+		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getWindowHandle());
+		
+		// identify the user name field pass in username
+		driver.findElement(By.xpath("//input[@id='header-search-input']")).sendKeys("xpath");
+		driver.findElement(By.xpath("//button[@id='header-desktop-search-button']")).click();
+		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getWindowHandle());
+		
+		Thread.sleep(2000);
+		driver.findElement(By.linkText("XPath Tutorial - W3Schools")).click();
+		
+		for(String i : driver.getWindowHandles()) {
+			System.out.println(i);
+			driver.switchTo().window(i);
+			}
+		
+		System.out.println(driver.getTitle());
+		driver.findElement(By.linkText("CSS")).click();
+		
+		
+		
+		driver.close();
+		driver.quit();
+}
+}
